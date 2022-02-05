ce499e98c9a46db13a0c764de0fb2db4c267a56e
BrandonC123 <brandoc318@gmail.com>
2022-02-05 11:04:10 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="header-item">Manage</div>
+        <div className="header-item">Build</div>
+      </div>
+      <div className="content">
+        <input type="text" className="title" placeholder="INSERT TITLE"></input>
+      </div>
+    </div>
+  );
+}
